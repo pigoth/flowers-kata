c90cde8bacca4b7c08a313333e1d3787453a1b27
Thomas Ferrari <thomas.ferrari@cherrychain.it>
2022-09-29 16:25:20 +0200

    sell single bundle

--- src/main/java/th/pigoth/flowers/Shop.java
+++ src/main/java/th/pigoth/flowers/Shop.java
@@ -1,6 +1,16 @@
 package th.pigoth.flowers;
 
+import java.math.BigDecimal;
+import java.util.Map;
+
 public class Shop {
+    private Map<Integer, BigDecimal> rosesBundles = Map.of(
+            5, BigDecimal.valueOf(6.99),
+            10, BigDecimal.valueOf(12.99)
+    );
 
 
+    public BigDecimal totalAmount(Integer nRoses, Integer nLilies, Integer nTulips) {
+        return rosesBundles.get(nRoses);
+    }
 }
--- src/test/java/th/pigoth/flowers/ShopTest.java
+++ src/test/java/th/pigoth/flowers/ShopTest.java
@@ -3,12 +3,23 @@ package th.pigoth.flowers;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShopTest {
 
     @Test
     @Disabled
-    void should_sell_single_product_and_get_result() {
+    void should_sell_single_roses_bundle() {
+        Shop shop = new Shop();
+        Integer nRoses = 10;
+        Integer nLilies = 0;
+        Integer nTulips = 0;
+
+        BigDecimal totalAmount = shop.totalAmount(nRoses, nLilies, nTulips);
+
+        assertThat(totalAmount).isEqualTo(BigDecimal.valueOf(12.99));
     }
 }
