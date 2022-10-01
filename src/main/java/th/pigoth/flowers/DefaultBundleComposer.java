6c16014fe152be011f9a7c35fd35b6b809b0d1bc
Thomas Ferrari <thomas.ferrari@cherrychain.it>
2022-10-01 10:21:21 +0200

    use min coins algorithm

--- src/main/java/th/pigoth/flowers/DefaultBundleComposer.java
+++ src/main/java/th/pigoth/flowers/DefaultBundleComposer.java
@@ -2,11 +2,17 @@ package th.pigoth.flowers;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+import static java.lang.Integer.MAX_VALUE;
 import static java.lang.String.format;
 import static java.math.BigDecimal.ZERO;
 import static java.math.BigDecimal.valueOf;
 import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toUnmodifiableList;
+import static java.util.stream.Stream.concat;
+import static java.util.stream.Stream.of;
 
 public class DefaultBundleComposer implements BundleComposer {
 
@@ -17,31 +23,64 @@ public class DefaultBundleComposer implements BundleComposer {
     }
 
     @Override
-    public String totalAmount(Integer nFlowers) {
+    public String totalAmount(Integer totalFlowers) {
+        int[] bundleCount = initializedBundleCount(totalFlowers);
+        HashMap<Integer, List<BundleComposition>> result = initializedResults(totalFlowers);
 
-        ArrayList<BundleComposition> result = new ArrayList<>();
-
-        flowerBundles.all().forEach(bundle -> {
-            var nFreeFlowers = nFlowers - result.stream().mapToInt(BundleComposition::totalFlowers).sum();
-            var bundleQuantity = nFreeFlowers / bundle.getSize();
-            result.add(new BundleComposition(bundle.getSize(), bundleQuantity, bundle.getAmount()));
-        });
-
-        return formatResult(nFlowers, result);
-
+        for (int nFlowers = 1; nFlowers <= totalFlowers; nFlowers++) {
+             for (Bundle bundle : flowerBundles.all()) {
+                boolean canUseBundle = bundle.getSize() <= nFlowers;
+                if (canUseBundle) {
+                    int flowerOutOfBundle = nFlowers - bundle.getSize();
+                    int flowerOutOfBundlePreviusComposition = bundleCount[flowerOutOfBundle];
+                    if (flowerOutOfBundlePreviusComposition != MAX_VALUE && flowerOutOfBundlePreviusComposition + 1 < bundleCount[nFlowers]) {
+                        bundleCount[nFlowers] = flowerOutOfBundlePreviusComposition + 1;
+                        result.put(nFlowers, bundleCompositionsFor(bundle, result.get(flowerOutOfBundle))) ;
+                    }
+                }
+            }
+        }
+        return formatResult(totalFlowers, result.get(totalFlowers));
     }
 
-    private String formatResult(Integer nFlowers, ArrayList<BundleComposition> result) {
+    private List<BundleComposition> bundleCompositionsFor(Bundle bundle, List<BundleComposition> flowerOutOfCurrentBundleComposition) {
+        if (flowerOutOfCurrentBundleComposition.stream().anyMatch(composition -> composition.bundleSize == bundle.getSize())) {
+            return flowerOutOfCurrentBundleComposition.stream()
+                    .map(composition -> composition.bundleSize == bundle.getSize() ? new BundleComposition(bundle, composition.bundleQuantity + 1) : composition)
+                    .collect(toUnmodifiableList());
+        } else {
+            return concat(flowerOutOfCurrentBundleComposition.stream(), of(new BundleComposition(bundle.getSize(), 1, bundle.getAmount())))
+                    .collect(toUnmodifiableList());
+        }
+    }
+
+    private static int[] initializedBundleCount(Integer totalFlowers) {
+        int[] table = new int[totalFlowers + 1];
+        table[0] = 0;
+        for (int i = 1; i <= totalFlowers; i++)
+            table[i] = MAX_VALUE;
+        return table;
+    }
+
+    private static HashMap<Integer, List<BundleComposition>> initializedResults(Integer totalFlowers) {
+        var result = new HashMap<Integer, List<BundleComposition>>();
+        result.put(0, new ArrayList<>());
+        for (int i = 1; i < totalFlowers + 1; i++) result.put(i, null);
+        return result;
+    }
+
+    private String formatResult(Integer nFlowers, List<BundleComposition> result) {
         BigDecimal totalAmount = result.stream()
                 .map(BundleComposition::totalAmount)
                 .reduce(ZERO, BigDecimal::add);
 
         String details = result.stream()
                 .filter(bundleComposition -> bundleComposition.bundleQuantity > 0)
+                .sorted((f1, f2) -> Integer.compare(f2.bundleSize, f1.bundleSize))
                 .map(BundleComposition::toString)
                 .collect(joining(" - ", "(", ")"));
 
-        return format("%d %s $%.2f %s", nFlowers, flowerBundles.flowerCode(),totalAmount, details);
+        return format("%d %s $%.2f %s", nFlowers, flowerBundles.flowerCode(), totalAmount, details);
     }
 
     private class BundleComposition {
@@ -56,8 +95,10 @@ public class DefaultBundleComposer implements BundleComposer {
             this.bundleAmount = bundleAmount;
         }
 
-        public int totalFlowers() {
-            return bundleSize * bundleQuantity;
+        public BundleComposition(Bundle bundle, Integer bundleQuantity) {
+            this.bundleSize = bundle.getSize();
+            this.bundleQuantity = bundleQuantity;
+            this.bundleAmount = bundle.getAmount();
         }
 
         public BigDecimal totalAmount() {
--- src/test/java/th/pigoth/flowers/DefaultBundleComposerTest.java
+++ src/test/java/th/pigoth/flowers/DefaultBundleComposerTest.java
@@ -9,44 +9,52 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class DefaultBundleComposerTest {
 
-    //    private final FlowerBundles tripleBundles = new FlowerBundles(
-//            "L09",
-//            List.of(new Bundle(3, valueOf(5.95)), new Bundle(5, valueOf(9.95)), new Bundle(9, valueOf(16.99)))
-//    );
-
     @Test
     void should_sell_more_roses_bundle() {
-        DefaultBundleComposer bundleComposer = new DefaultBundleComposer(new FlowerBundles(
+        var bundleComposer = new DefaultBundleComposer(new FlowerBundles(
                 "R12",
                 List.of(new Bundle(5, valueOf(6.99)), new Bundle(10, valueOf(12.99)))
         ));
 
-        String totalAmount = bundleComposer.totalAmount(20);
+        var totalAmount = bundleComposer.totalAmount(20);
 
         assertThat(totalAmount).isEqualTo("20 R12 $25.98 (2 x 10 $12.99)");
     }
 
     @Test
     void should_sell_different_type_of_bundles() {
-        DefaultBundleComposer bundleComposer = new DefaultBundleComposer(new FlowerBundles(
+        var bundleComposer = new DefaultBundleComposer(new FlowerBundles(
                 "R12",
                 List.of(new Bundle(5, valueOf(6.99)), new Bundle(10, valueOf(12.99)))
         ));
 
-        String totalAmount = bundleComposer.totalAmount(15);
+        var totalAmount = bundleComposer.totalAmount(15);
 
         assertThat(totalAmount).isEqualTo("15 R12 $19.98 (1 x 10 $12.99 - 1 x 5 $6.99)");
     }
 
     @Test
     void should_sell_bundles_with_proportional_three_level() {
-        DefaultBundleComposer bundleComposer = new DefaultBundleComposer(new FlowerBundles(
+        var bundleComposer = new DefaultBundleComposer(new FlowerBundles(
                 "L09",
                 List.of(new Bundle(3, valueOf(9.95)), new Bundle(6, valueOf(16.95)), new Bundle(9, valueOf(24.95)))
         ));
-        String totalAmount = bundleComposer.totalAmount(15);
+
+        var totalAmount = bundleComposer.totalAmount(15);
 
         assertThat(totalAmount).isEqualTo("15 L09 $41.90 (1 x 9 $24.95 - 1 x 6 $16.95)");
+    }
+
+    @Test
+    void should_sell_bundles_with_not_proportional_three_level() {
+        var bundleComposer = new DefaultBundleComposer(new FlowerBundles(
+                "T58",
+                List.of(new Bundle(3, valueOf(5.95)), new Bundle(5, valueOf(9.95)), new Bundle(9, valueOf(16.99)))
+        ));
+
+        String totalAmount = bundleComposer.totalAmount(13);
+
+        assertThat(totalAmount).isEqualTo("13 T58 $25.85 (2 x 5 $9.95 - 1 x 3 $5.95)");
     }
 
 }
